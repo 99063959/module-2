e1f58ac203ef672e4c3820f272c4b98b5f0c98e4
owen <99063959@mydavinci.nl>
2022-10-05 11:43:08 +0200

    speelhal

---
+++ speelhal.py
@@ -0,0 +1,14 @@
+aantal_mensen = 4
+toegangsprijs = 7.45
+vip = 0.37
+
+txt_1 = input("met hoeveel mensen ben je? ")
+aantal_1 = int(txt_1)
+print("je ben met dit aantal mensen", aantal_1)
+
+txt_2 = input("hoeveel minuten willen jullie in de vip? ")
+aantal_2 = int(txt_2)
+print("je wilt dit aantal minuten in de vip", aantal_2)
+
+bedrag = (4 * 7.45 + 9 * 0.37)
+print("het bedrag te betalen is ", bedrag)
