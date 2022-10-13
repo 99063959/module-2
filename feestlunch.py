1917b2a87fdfd595308b4663c30054b2b5ae49b1
owen <99063959@mydavinci.nl>
2022-10-13 09:40:11 +0200

    module2

--- feestlunch.py
+++ feestlunch.py
@@ -3,15 +3,12 @@ stokbroden = 2.78
 kortingsbon = 0.50
 
 aantal_1 = int (input("hoeveel croissants wilt uw? "))
-#aantal_1 = int(txt_1)
 print("uw heeft dit aantal croissants besteld", aantal_1)
 
-txt_2 = input("hoeveel stokbroden wilt uw? ")
-aantal_2 = int(txt_2)
+aantal_2 = int (input("hoeveel stokbroden wilt uw? "))
 print("uw heeft dit aantal stokbroden besteld", aantal_2)
 
-txt_3 = input("hoeveel kortingsbonnen heeft uw? ")
-aantal_3 = int(txt_3)
+aantal_3 = int (input("hoeveel kortingsbonnen heeft uw? "))
 print("uw heeft dit aantal kortingsbonnen", aantal_3)
 
 bedrag = (aantal_1 * croissant + aantal_2 * stokbroden + aantal_3 - kortingsbon)
--- speelhal.py
+++ speelhal.py
@@ -2,13 +2,11 @@ aantal_mensen = 4
 toegangsprijs = 7.45
 vip = 0.37
 
-txt_1 = input("met hoeveel mensen ben je? ")
-aantal_1 = int(txt_1)
+aantal_1 = int (input("met hoeveel mensen ben je? "))
 print("je ben met dit aantal mensen", aantal_1)
 
-txt_2 = input("hoeveel minuten willen jullie in de vip? ")
-aantal_2 = int(txt_2)
+aantal_2 = int (input("hoeveel minuten willen jullie in de vip? "))
 print("je wilt dit aantal minuten in de vip", aantal_2)
 
-bedrag = (aantal_mensen * toegangsprijs + aantal_2 * vip)
+bedrag = (aantal_1 * toegangsprijs + aantal_2 * vip)
 print("dit geweldige dagje met 4 mensen in de speelhal met 45 minuten VIP kostte me", bedrag, "euro")
