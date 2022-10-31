69779537a65a5ae83fd75936fd58aa862884d594
owen <99063959@mydavinci.nl>
2022-10-31 13:28:26 +0100

    person

---
+++ person.py
@@ -0,0 +1,11 @@
+naam = str(input("voer je naam in "))
+adres = str(input("voer je adres in "))
+postcode = str(input("voer je postcode in "))
+woonplaats = str(input("voer je woonplaats in "))
+
+print("------------------------------")
+print("|  Naam      : ", naam)
+print("|  Adres     : ", adres)
+print("|  Postcode  : ", postcode)
+print("|  Woonplaats: ", woonplaats)
+print("------------------------------")
