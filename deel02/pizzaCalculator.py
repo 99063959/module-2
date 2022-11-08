b10692f34e104336a399f9337c625ce4210d1e5b
owen <99063959@mydavinci.nl>
2022-11-08 13:51:05 +0100

    deel 2

--- deel02/pizzaCalculator.py
+++ deel02/pizzaCalculator.py
@@ -2,19 +2,15 @@ prijsSmall = 8
 prijsMedium = 9
 prijsLarge = 10
 notstop = True
+aantalSmall = 0
+aantalMedium = 0
+aantalLarge = 0
+bedragTotaal = 0
 
 while notstop:
     afmeting = input("wat voor maat pizza wil je small, medium of large? ")
-   
-    if afmeting == "stop":
-        notstop = False
-        bedragSmall = (aantalSmall * prijsSmall)
-        bedragMedium = (aantalMedium * prijsMedium)
-        bedragLarge = (aantalLarge * prijsLarge)
-        bedrag = (bedragSmall + bedragMedium + bedragLarge)
-        print("het bedrag wat je moet betalen voor de pizzas is" ,bedrag)
 
-    elif afmeting == "small":
+    if afmeting == "small":
         aantalSmall = int(input("hoeveel small pizzas wil je? "))
         print("je hebt",aantalSmall,"small pizzas besteld")
 
@@ -25,3 +21,10 @@ while notstop:
     elif afmeting == "large":
         aantalLarge = int(input("hoeveel large pizzas wil je? "))
         print("je hebt",aantalLarge,"large pizzas besteld")
+    
+    elif afmeting == "stop":
+        notstop = False
+        bedragTotaal += (aantalSmall * prijsSmall)
+        bedragTotaal += (aantalMedium * prijsMedium)
+        bedragTotaal += (aantalLarge * prijsLarge)
+        print("het totaal bedrag is",bedragTotaal,"euro")
