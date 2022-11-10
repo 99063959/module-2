5b1edeb0b95992c44db70b85cc3c919c3fd81d0f
owen <99063959@mydavinci.nl>
2022-11-10 11:03:06 +0100

    deel03

---
+++ deel03/kaasflow.py
@@ -0,0 +1,16 @@
+kaasGeel = input("is de kaas geel? ")
+
+if kaasGeel == "ja":
+    kaasGaten = input("zitten er gaten in? ")
+else:
+   kaasSchimmel = input("heeft de kaas blauwe schimmel? ")
+
+if kaasGaten == "ja":
+   kaasDuur = input("is de kaas belachelijk duur? ")
+else:
+   kaasSteen = input("is de kaas hard als steen? ")
+
+if kaasSchimmel == "ja":
+   kaasKorstRechts = input("heeft de kaas een korst? ")
+else:
+   kaasKorstLinks = input("heeft de kaas een korst? ")
---
+++ deel03/meten-is-weten.py
@@ -0,0 +1,15 @@
+getal1 = int(input("noem het eerste getal wat je wilt opgeven "))
+print("het eerste getal wat je hebt opgegeven is",getal1)
+
+getal2 = int(input("noem het tweede getal wat je wil opgeven "))
+print("het tweede getal wat je hebt opgegeven is",getal2)
+
+if getal1 > getal2:
+    print("het eerste getal",getal1,"is het grootste getal")
+
+elif getal2 > getal1:
+    print(getal1,"is kleiner dan het tweede getal",getal2)
+
+else:
+    print("het eerste en tweede getal zijn gelijk")
+
