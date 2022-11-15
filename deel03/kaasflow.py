46602241a00d053c2afb74df36c09f99d881edbb
owen <99063959@mydavinci.nl>
2022-11-15 13:53:24 +0100

    deel 3

--- deel03/kaasflow.py
+++ deel03/kaasflow.py
@@ -1,16 +1,43 @@
 kaasGeel = input("is de kaas geel? ")
 
 if kaasGeel == "ja":
-    kaasGaten = input("zitten er gaten in? ")
-else:
+   kaasGaten = input("zitten er gaten in? ")
+
+if kaasGeel == "nee":
    kaasSchimmel = input("heeft de kaas blauwe schimmel? ")
+
+if kaasSchimmel == "ja":
+   kaasRechts = input("heeft de kaas een korst? ")
+
+if kaasSchimmel == "nee":
+   kaasLinks = input("heeft de kaas een korst? ")
+
+if kaasLinks == "ja":
+   print("Camembert")
+
+if kaasLinks == "nee":
+   print("Mozzarella")
+
+if kaasRechts == "ja":
+   print("Blue de Rochbaron")
+
+if kaasRechts == "nee":
+   print("Foume d Ambert")
 
 if kaasGaten == "ja":
    kaasDuur = input("is de kaas belachelijk duur? ")
-else:
-   kaasSteen = input("is de kaas hard als steen? ")
 
-if kaasSchimmel == "ja":
-   kaasKorstRechts = input("heeft de kaas een korst? ")
-else:
-   kaasKorstLinks = input("heeft de kaas een korst? ")
+if kaasGaten == "nee":
+   kaasHard = input("is de kaas hard als steen? ")
+
+if kaasDuur == "ja":
+   print("Emmenthaler")
+
+if kaasDuur == "nee":
+   print("Leerdammer")
+
+if kaasHard == "ja":
+   print("Parmigiano Reggiano")
+
+if kaasHard == "nee":
+   print("Goudse Kaas")
---
+++ deel03/sollicitatie.py
@@ -0,0 +1,17 @@
+diploma = input("heb je een MBO 4 diploma? ")
+
+rijbewijs = input("heb je een geldig vrachtwagen rijbewijs? ")
+
+hoed = input("ben je in bezit van een hoed? ")
+
+manSnor = input("hoe breed is je snor? ")
+
+vrouwHaarKleur = input("welke kleur is je haar? ")
+
+vrouwHaarLengte = input("wat is de lengte van je haar in cm? ")
+
+zwaarte = input("wat is je lichaamsgewicht? ")
+
+certificaat = input("heb je het certificaat overleven met gevaarlijk personeel? ")
+
+ervaring = input("hoeveel jaar praktijkservaring heb je? ")
