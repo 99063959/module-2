8500b4ba456bf8075388d2b6e7dea1a1f9b08ac4
owen <99063959@mydavinci.nl>
2022-11-21 15:49:58 +0100

    deel03

--- deel03/sollicitatie.py
+++ deel03/sollicitatie.py
@@ -1,11 +1,5 @@
 diploma = input("heb je een MBO 4 diploma? ")
 
-rijbewijs = input("heb je een geldig vrachtwagen rijbewijs? ")
-
-hoed = input("ben je in bezit van een hoed? ")
-
-manSnor = input("hoe breed is je snor? ")
-
 vrouwHaarKleur = input("welke kleur is je haar? ")
 
 vrouwHaarLengte = input("wat is de lengte van je haar in cm? ")
@@ -15,3 +9,23 @@ zwaarte = input("wat is je lichaamsgewicht? ")
 certificaat = input("heb je het certificaat overleven met gevaarlijk personeel? ")
 
 ervaring = input("hoeveel jaar praktijkservaring heb je? ")
+
+if diploma == "ja":
+    rijbewijs = input("heb je een geldig vrachtwagen rijbewijs? ")
+else: 
+    rijbewijs = input("heb je een geldig vrachtwagen rijbewijs? ")
+
+if rijbewijs == "ja":
+    hoed = input("ben je in bezit van een hoed? ")
+else:
+    hoed = input("ben je in bezit van een hoed? ")
+
+if hoed == "ja":
+    manSnor = input("hoe breed is je snor? ")
+else:
+    manSnor = input("hoe breed is je snor? ")
+
+if manSnor == "....":
+    vrouwHaarKleur = input("welke kleur is je haar? ")
+else:
+    vrouwHaarKleur = input("welke kleur is je haar? ")
